beb55eb1f65869a924144580215a485f3bcf2ffc
Divyaperumal <divya3569@gmail.com>
2016-02-29 20:22:57 +0530

    Update sortingOfNumbersInStrings.c

--- sortingOfNumbersInStrings.c
+++ sortingOfNumbersInStrings.c
@@ -4,24 +4,24 @@
 
 int main(void)
 {
-  char *str[]={"1", "3", "11", "100", "19", "2", "5"},*temp;
+	char *str[]={"1", "3", "11", "100", "19", "2", "5"},*temp;
 	int i=0,j=0,n=7;
 	temp=(char*)malloc(8*sizeof(char));
-  for(i=0;i<n-1;i++)
+  	for(i=0;i<n-1;i++)
 	{
-	  for(j=i+1;j<n;j++)
-	  {
-		  if(strlen(str[i]) > strlen(str[j]) || strlen(str[i])==strlen(str[j]) &&  strcmp(str[i],str[j]) > 0)
-		  {
-		    temp =str[i];
-		    str[i]=str[j];
-		    str[j]=temp;
-		  }
-	  }
+		for(j=i+1;j<n;j++)
+	  	{
+			if(strlen(str[i]) > strlen(str[j]) || strlen(str[i])==strlen(str[j]) &&  strcmp(str[i],str[j]) > 0)
+		  	{
+		    		temp =str[i];
+		    		str[i]=str[j];
+		    		str[j]=temp;
+		  	}
+	  	}
 	}
 	for(i=0;i<n;i++)
 	{
-	   printf("%s\n",str[i]);
-  }
+		printf("%s\n",str[i]);
+  	}
 	return 0;
 }
